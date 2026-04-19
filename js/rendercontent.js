465d67a47911898147d86d5069eb85b792239919
downstreamspiral <hemendrakumararya0@gmail.com>
2026-04-20 04:01:03 +0530

    update

--- js/rendercontent.js
+++ js/rendercontent.js
@@ -3,7 +3,6 @@ let showimagetwo = document.getElementById('image2');
 let showvid = document.getElementById('videoplayer');
 
 // text render
-
 let tankname = document.getElementById('Tname');
 let countryname = document.getElementById('cname');
 let tanktype = document.getElementById('typeT');
@@ -15,58 +14,74 @@ let armorTurret = document.getElementById('armorTT');
 let armorHull = document.getElementById('armorHT');
 
 const selectedcountry = document.querySelectorAll(".countryselect");
-selectedcountry.forEach(selectedcountry => {
-    selectedcountry.addEventListener("click", function () {
+selectedcountry.forEach(item => {
+    item.addEventListener("click", function () {
         const activecountry = this.textContent;
-        console.log(activecountry);
-
-        renderMedia(activecountry);
-
-
-
-    })
-})
+        renderTankList(activecountry);
+    });
+});
 
 
-
-
-async function renderMedia(activecountry) {
-
+async function fetchData() {
     let catchJson = await fetch('info/render.json');
-    let recievedData = await catchJson.json();
-
-    // let activecountry = "Russia"; // holds object under a countryname
-    // change tank name dynamically
-    let tankoption = "T90A"; // holds keys under a tank name
-
-    // console.log(recievedData);
-    // console.log(activecountry);
-    // console.log(tankoption);
-    // console.log(recievedData[activecountry][tankoption].img);
-    // console.log(recievedData.Russia.Tankname);
-
-
-    // image and video render
-    showimageone.setAttribute("src", recievedData[activecountry][tankoption].img);
-    showimagetwo.setAttribute("src", recievedData[activecountry][tankoption].img2);
-    showvid.setAttribute("src", `https://www.youtube.com/embed/${recievedData[activecountry][tankoption].videoemb}`);
-
-    // text render
-    tankname.textContent = recievedData[activecountry][tankoption].Tankname;
-    countryname.textContent = recievedData[activecountry][tankoption].Countryname;
-    tanktype.textContent = recievedData[activecountry][tankoption].Tanktype;
-    servicetype.textContent = recievedData[activecountry][tankoption].Service;
-    companyname.textContent = recievedData[activecountry][tankoption].Companyname;
-
-    caliber.textContent = recievedData[activecountry][tankoption].caliber;
-    penTank.textContent = recievedData[activecountry][tankoption].pen;
-    armorTurret.textContent = recievedData[activecountry][tankoption].tarmor;
-    armorHull.textContent = recievedData[activecountry][tankoption].harmor;
-
-
-
-
-
-
+    return await catchJson.json();
 }
 
+
+async function renderTankList(activecountry) {
+    const recievedData = await fetchData();
+    const tankListEl = document.querySelector(".tank-list ul");
+
+    // get the keys for the selected country e.g ["T90A", "T72B", ...]
+    const tankKeys = Object.keys(recievedData[activecountry]);
+
+    // clear old list items and rebuild
+    tankListEl.innerHTML = "";
+
+    tankKeys.forEach(key => {
+        const li = document.createElement("li");
+        li.textContent = recievedData[activecountry][key].Tankname; // display friendly name
+        li.classList.add("tank-item");
+        li.dataset.tankKey = key; // store the key for lookup
+
+        li.addEventListener("click", function () {
+            // remove active class from all items then set on clicked
+            document.querySelectorAll(".tank-item").forEach(t => t.classList.remove("active-tank"));
+            this.classList.add("active-tank");
+
+            renderMedia(activecountry, key);
+        });
+
+        tankListEl.appendChild(li);
+    });
+
+
+    if (tankKeys.length > 0) {
+        tankListEl.firstChild.classList.add("active-tank");
+        renderMedia(activecountry, tankKeys[0]);
+    }
+}
+
+
+async function renderMedia(activecountry, tankoption) {
+    const recievedData = await fetchData();
+    const tank = recievedData[activecountry][tankoption];
+
+    // image and video render
+    showimageone.setAttribute("src", tank.img);
+    showimagetwo.setAttribute("src", tank.img2);
+    showvid.setAttribute("src", `https://www.youtube.com/embed/${tank.videoemb}`);
+
+    // text render
+    tankname.textContent = tank.Tankname;
+    countryname.textContent = tank.Countryname;
+    tanktype.textContent = tank.Tanktype;
+    servicetype.textContent = tank.Service;
+    companyname.textContent = tank.Companyname;
+    caliber.textContent = tank.caliber;
+    penTank.textContent = tank.pen;
+    armorTurret.textContent = tank.tarmor;
+    armorHull.textContent = tank.harmor;
+}
+
+renderTankList("Russia");
